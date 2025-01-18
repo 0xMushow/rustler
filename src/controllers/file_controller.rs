b825dc3fc73f5d634865b9cff8939f34776094e3
Antoine James <mushow@proton.me>
2025-01-18 19:39:58 +0100

    feat(file_controller): add view_codebase_handler to handle downloading and extracting zip files

--- src/controllers/file_controller.rs
+++ src/controllers/file_controller.rs
@@ -1,8 +1,8 @@
-use axum::{
-    extract::{Multipart, State},
-    response::IntoResponse,
-};
+use axum::{extract::{Multipart, State}, response::IntoResponse, Json};
 use std::sync::Arc;
+use axum::extract::Path;
+use axum::http::StatusCode;
+use serde_json::json;
 use crate::clients::clients::Clients;
 use crate::services::file_service::FileService;
 
@@ -21,4 +21,18 @@ pub async fn upload_handler(
 ) -> impl IntoResponse {
     let file_service = FileService::new(clients);
     file_service.upload_file(multipart).await
+}
+
+pub async fn view_codebase_handler(
+    State(clients): State<Arc<Clients>>,
+    Path(name): Path<String>,
+) -> impl IntoResponse {
+    let file_service = FileService::new(clients);
+
+    let output_dir = format!("./competitions/{}", name);
+
+    match file_service.download_and_extract_zip(&name, &output_dir).await {
+        Ok(files) => (StatusCode::OK, Json(json!({ "files": files }))).into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({ "error": e.to_string() }))).into_response(),
+    }
 }
