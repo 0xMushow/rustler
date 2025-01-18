da6fcab71bbff3d771f8d7c9057431e48bd04304
Antoine James <mushow@proton.me>
2025-01-18 19:35:55 +0100

    feat(error): added SDK, Zip, Std::File errors

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,10 @@ use aws_sdk_s3::operation::list_objects_v2::ListObjectsV2Error;
 use aws_sdk_s3::error::SdkError;
 use sqlx::Error as SqlxError;
 use redis::RedisError;
+use std::io;
+use aws_sdk_s3::config::http::HttpResponse;
+use aws_sdk_s3::operation::get_object::GetObjectError;
+use aws_sdk_s3::primitives::ByteStreamError;
 
 /// Represents custom errors that can occur in the application.
 ///
@@ -26,4 +30,24 @@ pub enum AppError {
     /// An error indicating a failure to connect to or interact with a Redis server.
     #[error("Redis connection error: {0}")]
     RedisConnectionError(#[from] RedisError),
+
+    /// An error indicating a failure during file validation or extraction.
+    #[error("File validation or extraction error: {0}")]
+    ValidationError(String),
+
+    /// An error indicating a failure during file I/O operations.
+    #[error("File I/O error: {0}")]
+    FileIoError(#[from] io::Error),
+
+    /// An error indicating a failure during ZIP file extraction.
+    #[error("ZIP extraction error: {0}")]
+    ZipError(#[from] zip::result::ZipError),
+
+    /// An error indicating a failure during S3 object download.
+    #[error("Sdk Download Object Error: {0}")]
+    SdkDownloadObjectError(#[from] SdkError<GetObjectError, HttpResponse>),
+
+    /// An error indicating a failure during byte stream operations.
+    #[error("Byte Stream Error: {0}")]
+    ByteStreamError(#[from] ByteStreamError),
 }
