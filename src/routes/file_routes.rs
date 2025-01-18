1a032251ebd1e4f19da4d5b3c7b2cf8c5f468aa1
Antoine James <mushow@proton.me>
2025-01-18 19:36:55 +0100

    feat(route): added /view-codebase/{name} endpoint

--- src/routes/file_routes.rs
+++ src/routes/file_routes.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
-use axum::{Router, routing::post};
+use axum::{Router, routing::{post, get}};
 use axum::extract::DefaultBodyLimit;
 use crate::clients::clients::Clients;
-use crate::controllers::file_controller::upload_handler;
+use crate::controllers::file_controller::{upload_handler, view_codebase_handler};
 
 /// Defines the file routes.
 ///
@@ -11,10 +11,13 @@ use crate::controllers::file_controller::upload_handler;
 ///
 /// # Returns
 /// A Router containing the file routes.
-/// Disable the default body limit to allow large file uploads.
+/// Disable the default body limit for the `/upload` route to allow large file uploads.
 ///
 pub fn file_routes(state: Arc<Clients>) -> Router {
-    Router::new().route("/upload", post(upload_handler)
-        .layer(DefaultBodyLimit::disable())
-        .with_state(state))
+    Router::new()
+        .route("/upload", post(upload_handler)
+            .layer(DefaultBodyLimit::disable())
+            .with_state(state.clone()))
+        .route("/view-codebase/{name}", get(view_codebase_handler)
+            .with_state(state))
 }
