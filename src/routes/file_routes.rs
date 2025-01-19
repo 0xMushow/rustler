8283d9996c37d8f6c198ec22dc1212b949a09a35
Antoine James <mushow@proton.me>
2025-01-19 16:35:39 +0100

    feat(file-routes): added generate-codebase-json route

--- src/routes/file_routes.rs
+++ src/routes/file_routes.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 use axum::{Router, routing::{post, get}};
 use axum::extract::DefaultBodyLimit;
+use axum::handler::Handler;
 use crate::clients::clients::Clients;
-use crate::controllers::file_controller::{upload_handler, view_codebase_handler};
+use crate::controllers::file_controller::{generate_codebase_json, upload_handler, view_codebase_handler};
 
 /// Defines the file routes.
 ///
@@ -19,5 +20,7 @@ pub fn file_routes(state: Arc<Clients>) -> Router {
             .layer(DefaultBodyLimit::disable())
             .with_state(state.clone()))
         .route("/view-codebase/{name}", get(view_codebase_handler)
+            .with_state(state.clone()))
+        .route("/generate-codebase-json/{name}", get(generate_codebase_json)
             .with_state(state))
 }
